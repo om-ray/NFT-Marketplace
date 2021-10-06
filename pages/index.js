e62deaa03b1ca606886bd39350dccea884729f8b
123om123 <omihridesh@gmail.com>
2021-10-06 18:48:37 -0400

    re-deploy

--- pages/index.js
+++ pages/index.js
@@ -164,8 +164,6 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
-        <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-        <script src="https://cdn.jsdelivr.net/npm/react-apexcharts"></script>
       </Head>
 
       <div className={styles.container}>
