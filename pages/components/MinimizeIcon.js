c231991b9706dd9824687dc9bf8f7715dbdb278c
123om123 <omihridesh@gmail.com>
2021-10-19 14:59:13 -0400

    re-redeploy

--- pages/components/MinimizeIcon.js
+++ pages/components/MinimizeIcon.js
@@ -12,7 +12,7 @@ function MinimizeIcon() {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      class="feather feather-minimize-2">
+      className="feather feather-minimize-2">
       <polyline points="4 14 10 14 10 20"></polyline>
       <polyline points="20 10 14 10 14 4"></polyline>
       <line x1="14" y1="10" x2="21" y2="3"></line>
