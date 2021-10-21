929c480f960830a4345c3affabab22913aee5a17
123om123 <omihridesh@gmail.com>
2021-10-21 15:34:40 -0400

    Big graph finaly shows all data

--- pages/index.js
+++ pages/index.js
@@ -14,20 +14,28 @@ let dayGroupArr = [];
 let groupArr = [];
 let avgCollectionBuyPrice;
 let currentCollection = false;
+let currentCollectionAddress = false;
 let totalLoopsNeeded;
 let initalQueryOffset = 0;
 let DBUrl = "http://localhost:5000/graphql";
+let test = 0;
 
 let interval = 86400000;
 let address; /* = "0x7e99430280a0640a4907ccf9dc16c3d41be6e1ed"; */
 
 export default function Home() {
-  let [balance, setBalance] = useState(null);
-  let [NFTS, setNFTS] = useState(null);
-  let [options, setOptions] = useState({});
-  let [series, setSeries] = useState([{}]);
-  let [updates, setUpdates] = useState(null);
-  let [expandGraph, setExpandGraph] = useState(false);
+  let [updates, setUpdates] = useState(() => null);
+  let [balance, setBalance] = useState(() => null);
+  let [NFTS, setNFTS] = useState(() => null);
+  let [options, setOptions] = useState(() => {
+    return {};
+  });
+  let [series, setSeries] = useState(() => [{}]);
+  let [bigoptions, setBigOptions] = useState(() => {
+    return {};
+  });
+  let [bigseries, setBigSeries] = useState(() => [{}]);
+  let [expandGraph, setExpandGraph] = useState(() => false);
   let value;
   let web3 = new Web3("https://speedy-nodes-nyc.moralis.io/1c58af41ad51021daa7433bb/eth/mainnet");
 
@@ -121,49 +129,168 @@ export default function Home() {
       tokens.json().then((tokens) => {
         totalLoopsNeeded = Math.ceil((tokens.total - initalQueryOffset) / 500);
         console.log("populating db", totalLoopsNeeded);
-        for (let i = 0; i <= totalLoopsNeeded; i++) {
-          let queryOffset = i * 500 + initalQueryOffset;
-          fetch(
-            `https://deep-index.moralis.io/api/v2/nft/${collection_addr}/transfers?chain=eth&format=decimal&offset=${queryOffset}&order=block_timestamp.DESC`,
-            {
-              method: "GET",
-              headers: {
-                "content-type": "application/json; charset=utf-8",
-                "x-api-key": "Q3Zg3JYiD2uaEbeyeVtHVOfdQDN2ERvqqVX7M15HHa2kXq1uBIy1BpM9hk918OLV",
-              },
-            }
-          ).then((tokens) => {
-            tokens.json().then((tokens) => {
-              tokens.result.map((token) => {
-                fetch(DBUrl, {
-                  method: "POST",
-                  headers: { "Content-Type": "application/json" },
-                  body: JSON.stringify({
-                    query: `
-                  mutation MyMutation {
-                    createNftDatum(input: {nftDatum: {value: "${token.value}", timestamp: "${
-                      token.block_timestamp.split("T")[0]
-                    }", tokenAddress: "${token.token_address}"}}) {
-                      nftDatum {
-                        timestamp
-                        tokenAddress
-                        value
-                      }
-                    }
-                  }`,
-                  }),
-                }).then((NFTData) => {
-                  NFTData.json().then((NFTData) => {});
+        let i = 0;
+        var runMutation = setInterval(() => {
+          if (i <= totalLoopsNeeded) {
+            let queryOffset = i * 500 + initalQueryOffset;
+            fetch(
+              `https://deep-index.moralis.io/api/v2/nft/${collection_addr}/transfers?chain=eth&format=decimal&offset=${queryOffset}&order=block_timestamp.DESC`,
+              {
+                method: "GET",
+                headers: {
+                  "content-type": "application/json; charset=utf-8",
+                  "x-api-key": "Q3Zg3JYiD2uaEbeyeVtHVOfdQDN2ERvqqVX7M15HHa2kXq1uBIy1BpM9hk918OLV",
+                },
+              }
+            ).then((tokens) => {
+              tokens
+                .json()
+                .then((tokens) => {
+                  tokens.result.map(async (token) => {
+                    await fetch(DBUrl, {
+                      method: "POST",
+                      headers: { "Content-Type": "application/json" },
+                      body: JSON.stringify({
+                        query: `
+                          mutation MyMutation {
+                            createNftDatum(input: {nftDatum: {value: "${token.value}", timestamp: "${token.block_timestamp}", tokenAddress: "${token.token_address}"}}) {
+                              nftDatum {
+                                timestamp
+                                tokenAddress
+                                value
+                              }
+                            }
+                          }`,
+                      }),
+                    })
+                      .then((NFTData) => {
+                        NFTData.json()
+                          .then((NFTData) => {
+                            console.log(NFTData);
+                          })
+                          .catch((err) => {
+                            console.error(err);
+                          });
+                      })
+                      .catch((err) => {
+                        console.error(err);
+                      });
+                  });
+                })
+                .catch((err) => {
+                  console.error(err);
                 });
-              });
             });
-          });
-        }
+            i += 1;
+          } else {
+            clearInterval(runMutation);
+          }
+        }, 1000);
       });
     });
   };
 
-  let makeAvgBuyPriceGraph = function (tokens, collection_name) {
+  let makeBigAvgBuyPriceGraph = function (collection_addr) {
+    seriesArr = [];
+    groupArr = [];
+    fetch(DBUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `query MyQuery {
+          allNftData(
+            condition: {tokenAddress: "${collection_addr}"}
+            orderBy: TIMESTAMP_DESC
+          ) {
+            nodes {
+              timestamp
+              tokenAddress
+              value
+            }
+          }
+        }`,
+      }),
+    })
+      .then((tokens) => {
+        timeArr = [];
+        collectionBuyPriceArr = [];
+        tokens
+          .json()
+          .then((tokens) => {
+            timeArr = [];
+            collectionBuyPriceArr = [];
+            tokens.data.allNftData.nodes.map((token) => {
+              timeArr.push(token.timestamp.split("T")[0]);
+              collectionBuyPriceArr.push([token.timestamp.split("T")[0], JSON.parse(token.value)]);
+              if (collectionBuyPriceArr.length == tokens.data.allNftData.nodes.length) {
+                timeArr = timeArr.filter(onlyUnique);
+                timeArr.sort(function (x, y) {
+                  return x - y;
+                });
+                timeArr.reverse();
+                collectionBuyPriceArr.sort(function (x, y) {
+                  return x[0] - y[0];
+                });
+                collectionBuyPriceArr.reverse();
+                for (let i in collectionBuyPriceArr) {
+                  let token = collectionBuyPriceArr[i];
+                  let lastTimestamp = collectionBuyPriceArr[i - 1]
+                    ? collectionBuyPriceArr[i - 1][0]
+                    : token[0];
+                  if (token[0]) {
+                    if (token[0] == lastTimestamp) {
+                      dayGroupArr.push(token[1]);
+                    } else if (token[0] !== lastTimestamp) {
+                      groupArr.push(dayGroupArr);
+                      dayGroupArr = [];
+                      dayGroupArr.push(token[1]);
+                      if (token[0] == collectionBuyPriceArr[collectionBuyPriceArr.length - 1][0]) {
+                        groupArr.push(dayGroupArr);
+                      }
+                    }
+                  }
+                }
+                for (let i in groupArr) {
+                  let group = groupArr[i];
+                  group = average(group);
+                  seriesArr.push(web3.utils.fromWei(group.toString(), "ether"));
+                  console.log(timeArr, seriesArr);
+                }
+              }
+            });
+            setBigOptions({
+              xaxis: {
+                categories: timeArr,
+              },
+              colors: ["#00c234"],
+              fill: {
+                type: "gradient",
+                gradient: {
+                  shadeIntensity: 1,
+                  opacityFrom: 0.7,
+                  opacityTo: 0.9,
+                  stops: [0, 100],
+                },
+              },
+              noData: {
+                text: "No data to show",
+                align: "center",
+                verticalAlign: "middle",
+              },
+            });
+            setBigSeries([{ name: currentCollection, data: seriesArr }]);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  let makeSmallAvgBuyPriceGraph = function (tokens, collection_name) {
+    currentCollection = collection_name;
     tokens.json().then((tokens) => {
       tokens.result.map((token) => {
         timeArr.push(token.block_timestamp.split("T")[0]);
@@ -177,6 +304,7 @@ export default function Home() {
           collectionBuyPriceArr.sort(function (x, y) {
             return x[0] - y[0];
           });
+          collectionBuyPriceArr.reverse();
           for (let i in collectionBuyPriceArr) {
             let token = collectionBuyPriceArr[i];
             let lastTimestamp = collectionBuyPriceArr[i - 1] ? collectionBuyPriceArr[i - 1][0] : token[0];
@@ -199,6 +327,7 @@ export default function Home() {
             let group = groupArr[i];
             group = average(group);
             seriesArr.push(web3.utils.fromWei(group.toString(), "ether"));
+            console.log(seriesArr);
           }
         }
       });
@@ -217,7 +346,7 @@ export default function Home() {
           },
         },
         noData: {
-          test: "No data to show",
+          text: "No data to show",
           align: "center",
           verticalAlign: "middle",
         },
@@ -230,6 +359,7 @@ export default function Home() {
     return function () {
       let collectionChanged;
       populateNFTDataDB(collection_addr);
+      currentCollectionAddress = collection_addr;
       console.log(collection_addr);
       fetch(
         `https://deep-index.moralis.io/api/v2/nft/${collection_addr}/transfers?chain=eth&format=decimal&order=block_timestamp.DESC`,
@@ -254,7 +384,7 @@ export default function Home() {
           currentCollection = collection_name;
         }
 
-        makeAvgBuyPriceGraph(tokens, collection_name);
+        makeSmallAvgBuyPriceGraph(tokens, collection_name);
       });
 
       var autoCall = setInterval(function () {
@@ -280,7 +410,7 @@ export default function Home() {
             currentCollection = collection_name;
           }
 
-          makeAvgBuyPriceGraph(tokens, collection_name);
+          makeSmallAvgBuyPriceGraph(tokens, collection_name);
         });
       }, interval);
     };
@@ -454,12 +584,12 @@ export default function Home() {
                   border: "1px solid #dedede",
                   borderRadius: "20px",
                 }}>
-                <div style={{ width: "90%", height: "60%" }}>
+                <div onload={() => {}} style={{ width: "90%", height: "60%" }}>
                   <h1>{currentCollection} Avg. Buy price</h1>
                   <Chart
                     id="floorPriceChart"
-                    options={options}
-                    series={series}
+                    options={bigoptions}
+                    series={bigseries}
                     type="area"
                     width="100%"
                     height="100%"></Chart>
@@ -481,6 +611,7 @@ export default function Home() {
               <button
                 onClick={() => {
                   setExpandGraph(true);
+                  makeBigAvgBuyPriceGraph(currentCollectionAddress);
                 }}
                 style={{ margin: "0 0 0 1rem", float: "right", fontSize: "10px", padding: "0.5rem 2rem" }}
                 className={styles.btnBig}>
