397cdfbafe0dd312b4686f522cd17db97fe9df68
123om123 <omihridesh@gmail.com>
2022-01-30 13:10:34 +0530

    updated postgraphile path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "postgraphile",
-      script: "/usr/local/bin/postgraphile",
+      script: "/home/ec2-user/.nvm/versions/node/v17.4.0/bin/postgraphile",
       args: "-c postgres://postgres:postgres@nft-data.ctebvyvrwlrm.ap-south-1.rds.amazonaws.com:5432/postgres --host 0.0.0.0 --watch --port 5000 --enhance-graphiql --dynamic-json -o",
       // "instances": 1,
       // "exec_mode": "cluster",
