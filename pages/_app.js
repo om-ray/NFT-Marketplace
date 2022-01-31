01e3af657cd9b98acbfec4c78358624032d5e983
123om123 <omihridesh@gmail.com>
2022-01-31 11:14:10 +0530

    adblock detection

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <head>
+        <script src="./ads-prebid.js"></script>
+        <script src="./ads.js"></script>
+        <script src="./prebid-ads.js"></script>
+      </head>
+      {(function () {
+        if (
+          window.canRunAds1 === undefined ||
+          window.canRunAds2 === undefined ||
+          window.canRunAds3 === undefined
+        ) {
+          window.alert(
+            "Please dsable any adblockers you have enabled, they stop this application from working as it should"
+          );
+        }
+      })()}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
