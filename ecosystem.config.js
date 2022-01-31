6452ab5eb504abc584c0e725f7d5d7843d584b94
123om123 <omihridesh@gmail.com>
2022-01-31 10:58:52 +0530

    removed the ngrok script from ecosystem file because it causes errors

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,6 @@ module.exports = {
       script: "/home/ec2-user/.nvm/versions/node/v17.4.0/bin/postgraphile",
       args: "-c postgres://postgres:postgres@nft-data.ctebvyvrwlrm.ap-south-1.rds.amazonaws.com:5432/postgres --host 0.0.0.0 --watch --port 5000 --enhance-graphiql --dynamic-json -o --cors",
     },
-    {
-      name: "ngrok",
-      script: "ngrok",
-      args: "ngrok http --region=us --hostname=nft-marketplace.ngrok.io 3000",
-    },
   ],
 
   deploy: {
