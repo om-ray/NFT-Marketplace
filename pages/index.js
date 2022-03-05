719053eb07ca166b3da8db8a97287df460000c23
123om123 <omihridesh@gmail.com>
2022-03-05 23:48:29 +0530

    test

--- pages/index.js
+++ pages/index.js
@@ -547,7 +547,6 @@ export default function Home() {
 
     if (user) {
       await findAccount();
-      console.log(process.env.AUTH0_SECRET);
       setLoggedIn(true);
     }
   }, [user]);
@@ -751,6 +750,7 @@ export default function Home() {
                 backgroundColor: "rgba(255,255,255,0)",
               }}>
               <a href="/api/auth/login">Login</a>
+              {console.log(process.env.AUTH0_SECRET)}
             </button>
             {/* <div className={styles.accountInfoWrapper}>
               <input
